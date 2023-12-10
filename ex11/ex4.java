d54cfe671a351ee21e5a82faf036ea86a56cdac1
Artem1Fomin <144795499+Artem1Fomin@users.noreply.github.com>
2023-12-10 19:22:00 +0300

    Add files via upload

---
+++ ex11/ex4.java
@@ -0,0 +1,30 @@
+package ex11;
+import java.util.Calendar;
+import java.util.Date;
+public class ex4 {
+
+        public static void main(String[] args) {
+            DateGen dg = new DateGen(2023, 3, 13, 13, 00, 54);
+            dg.generate();
+        }
+    }
+    class DateGen{
+        int[] elements = new int[6];
+        DateGen(int... elements){
+            for (int i = 0; i < this.elements.length; i++) {
+                this.elements[i] = elements[i];
+            }
+        }
+        void generate(){
+            Calendar cal = Calendar.getInstance();
+            cal.clear();
+            cal.set(Calendar.YEAR, elements[0]);
+            cal.set(Calendar.MONTH, elements[1]);
+            cal.set(Calendar.DAY_OF_MONTH, elements[2]);
+            cal.set(Calendar.HOUR_OF_DAY, elements[3]);
+            cal.set(Calendar.MINUTE, elements[4]);
+            cal.set(Calendar.SECOND, elements[5]);
+
+            System.out.println(cal.getTime());
+        }
+}
